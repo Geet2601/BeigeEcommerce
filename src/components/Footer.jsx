ae0ae5603b21a36f156c6868e9c4149bd53e8755
Geet2601 <gt2601001gt@gmail.com>
2023-12-16 21:00:04 +0530

    some minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,47 @@
-  import "./Footer.css";
+import "./Footer.css";
 
-  function Footer() {
-    return (
-      <>
-        <footer>
-        
-          <div className="footer-links">
-            <button type="button" onClick={() => window.location.href = 'https://geet2601.github.io/E--Portfolio/'}>ABOUT</button>
-            <button type="button" onClick={() => window.location.href = '#'}>
-            Store locator
-          </button>
+function Footer() {
+  const openLinkInNewTab = (url) => {
+    window.open(url, "_blank");
+  };
+
+  return (
+    <>
+      <footer>
+      
+        <div className="footer-links">
+          <button type="button" onClick={() => openLinkInNewTab('https://geet2601.github.io/E--Portfolio/')}>ABOUT</button>
           <button type="button" onClick={() => window.location.href = '#'}>
-            FAQs
-          </button>
-          <button type="button" onClick={() => window.location.href = '#'}>
-            News
-          </button>
-          <button type="button" onClick={() => window.location.href = '#'}>
-            Careers
-          </button>
-          <button type="button" onClick={() => window.location.href = '#'}>
-            Contact Us
-          </button>
-        </div>
-        <p className="love">
+          Store locator
+        </button>
+        <button type="button" onClick={() => window.location.href = '#'}>
+          FAQs
+        </button>
+        <button type="button" onClick={() => window.location.href = '#'}>
+          News
+        </button>
+        <button type="button" onClick={() => window.location.href = '#'}>
+          Careers
+        </button>
+        <button type="button" onClick={() => window.location.href = '#'}>
+          Contact Us
+        </button>
+      </div>
+      <p className="love">
+        &nbsp;
+        {/* <button
+          type="button"
+          onClick={() => {
+            // Handle click action for the love button
+          }}
+          style={{ color: "beige" }}
+        >
           &nbsp;
-          <button
-            type="button"
-            onClick={() => {
-              // Handle click action for the love button
-            }}
-            style={{ color: "white" }}
-          >
-            &nbsp;
-          </button>
-        </p>
-      </footer>
-    </>
-  );
+        </button> */}
+      </p>
+    </footer>
+  </>
+);
 }
 
 export default Footer;
