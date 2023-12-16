8ab3c09292d0de43cb31e958a97be36f0af764f0
Geet2601 <gt2601001gt@gmail.com>
2023-12-16 16:22:25 +0530

    hrefs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ function Footer() {
     <>
       <footer>
         <div className="footer-links">
-          <a href="/">About</a>
-          <a href="/">Store locator</a>
-          <a href="/">FAQs</a>
-          <a href="/">News</a>
-          <a href="/">Careers</a>
-          <a href="/">Contact Us</a>
+          <a href="https://geet2601.github.io/E--Portfolio/">About</a>
+          <a href="https://geet2601.github.io/E--Portfolio/">Store locator</a>
+          <a href="https://geet2601.github.io/E--Portfolio/">FAQs</a>
+          <a href="https://geet2601.github.io/E--Portfolio/">News</a>
+          <a href="https://geet2601.github.io/E--Portfolio/">Careers</a>
+          <a href="https://geet2601.github.io/E--Portfolio/">Contact Us</a>
         </div>
         <p className="love">
           &nbsp;{" "}
@@ -18,8 +18,7 @@ function Footer() {
             target="_blank"
             rel="noreferrer"
             style={{ color: "white" }}
-            
-          >
+         >
             &nbsp;
           </a>
         </p>
