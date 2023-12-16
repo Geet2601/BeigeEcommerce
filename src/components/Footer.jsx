8497cac0f38a591a98bbd730fa79615959df2028
Geet2601 <gt2601001gt@gmail.com>
2023-12-16 15:18:13 +0530

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <footer>
         <div className="footer-links">
           <a href="/">About</a>
-          <a href="">Store locator</a>
+          <a href="/">Store locator</a>
           <a href="/">FAQs</a>
           <a href="/">News</a>
           <a href="/">Careers</a>
