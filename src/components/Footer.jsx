ead715168dbbb4a361eb82253716887953ae8316
Geet2601 <gt2601001gt@gmail.com>
2023-12-16 20:09:31 +0530

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ function Footer() {
     <>
       <footer>
         <div className="footer-links">
-          {/* <a href="https://geet2601.github.io/E--Portfolio/">About</a>
-          <a href="https://geet2601.github.io/E--Portfolio/">Store locator</a>
-          <a href="https://geet2601.github.io/E--Portfolio/">FAQs</a>
-          <a href="https://geet2601.github.io/E--Portfolio/">News</a>
-          <a href="https://geet2601.github.io/E--Portfolio/">Careers</a>
-          <a href="https://geet2601.github.io/E--Portfolio/">ContactUs</a> */}
+          <a href="https://geet2601.github.io/E--Portfolio/">About</a>
+          <a href="#">Store locator</a>
+          <a href="#">FAQs</a>
+          <a href="#">News</a>
+          <a href="#">Careers</a>
+          <a href="#">ContactUs</a>
         </div>
         <p className="love">
           &nbsp;{" "}
